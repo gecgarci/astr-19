b1b65ad025c2774a14207be69c3a14fefbd0af5e
Genevieve <146879581+gecgarci@users.noreply.github.com>
2023-10-06 16:13:39 -0700

    Add files via upload

---
+++ code-journal-1.py
@@ -0,0 +1,9 @@
+full_name = "Genevieve Garcia"
+pronouns = "\npronouns: she/her"
+
+a = (full_name + pronouns)
+print(a)
+
+b = "My favorite movie is Doom Generation by Gregg Akari. "
+c = "My favorite food is probably potato samosas"
+print(b+c)
